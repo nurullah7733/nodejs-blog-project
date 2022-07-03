37b0a0b234bb9dd5c8987532a46ed99da25ca0f9
nurullah <nurullah7733@gmail.com>
2022-07-03 17:03:18 +0600

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,21 @@
-const UserModel = require('../models/User.js');
+// const UserModel = require('../models/User.js');
 
-exports.bindUserWithRequest = () => {
-    return async (req, res, next) => {
-        if (!req.session.isLoggedIn) {
-            return next();
-        }
+// exports.bindUserWithRequest = () => {
+//     return async (req, res, next) => {
+//         if (!req.session.isLoggedIn) {
+//             return next();
+//         }
 
-        try {
-            let user = await UserModel.findById(req.session.user._id);
-            req.user = user;
-            next();
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    };
-};
+//         try {
+//             let user = await UserModel.findById(req.session.user._id);
+//             req.user = user;
+//             next();
+//         } catch (error) {
+//             console.log(error);
+//             next(error);
+//         }
+//     };
+// };
 
 exports.isAuthenticated = (req, res, next) => {
     if (!req.session.isLoggedIn) {
